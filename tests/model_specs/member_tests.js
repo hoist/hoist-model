427013294a9270113f02ccbda62eaed001cc299b
Owen Evans <owen@bgeek.net>
2014-10-03 13:40:07 +1300

    add some tests

--- tests/model_specs/member_tests.js
+++ tests/model_specs/member_tests.js
@@ -23,21 +23,76 @@ describe('Member', function () {
   describe('validation', function () {
     var member;
     before(function () {
-      member = new Member({});
+      member = new Member({
+        application:'appid',
+        environment:'live'
+      });
     });
     describe('save', function () {
       var saved;
       before(function () {
-        saved = new Member({
-          Application: 'appid'
-        }).saveAsync();
+        saved = member.saveAsync();
       });
-      it('should save', function () {
+      it('is saved', function () {
         return expect(saved).to.be.fulfilled;
+      });
+      it('has id',function(){
+        return expect(saved.then(function(member){
+          return member._id.length;
+        })).to.become(20);
+      });
+    });
+    describe('application',function(){
+      afterEach(function(){
+        member.application = 'appid';
+      });
+      it('is required',function(){
+        member.application = null;
+        return member.validateAsync().then(function () {
+          //fail here;
+          expect(false).to.be.true();
+        }).catch(function (err) {
+          expect(err).to.be.instanceof(mongoose.Error.ValidationError);
+          /* jshint -W030 */
+          expect(err.errors.application).to.exist;
+          expect(err.errors.application.message).to.eql('members must belong to an application');
+        });
+      });
+    });
+     describe('environment',function(){
+      afterEach(function(){
+        member.environment = 'live';
+      });
+      it('is required',function(){
+        member.environment = null;
+        return member.validateAsync().then(function () {
+          //fail here;
+          expect(false).to.be.true();
+        }).catch(function (err) {
+          expect(err).to.be.instanceof(mongoose.Error.ValidationError);
+          /* jshint -W030 */
+          expect(err.errors.environment).to.exist;
+          expect(err.errors.environment.message).to.eql('members must belong to a valid environment');
+        });
+      });
+      it('it must be a valid environment',function(){
+        member.environment = 'random';
+        return member.validateAsync().then(function () {
+          //fail here;
+          expect(false).to.be.true();
+        }).catch(function (err) {
+          expect(err).to.be.instanceof(mongoose.Error.ValidationError);
+          /* jshint -W030 */
+          expect(err.errors.environment).to.exist;
+          expect(err.errors.environment.message).to.eql('members must belong to a valid environment');
+        });
       });
     });
     describe('email', function () {
       beforeEach(function () {
+        member.emailAddresses = [];
+      });
+      afterEach(function(){
         member.emailAddresses = [];
       });
       it('allows valid email', function () {
