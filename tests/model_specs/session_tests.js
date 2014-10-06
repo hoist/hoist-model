f02adf7caaea41f6f5d9d7e58bade768f7b76a28
Owen Evans <owen@bgeek.net>
2014-10-06 16:14:23 +1300

    add session

---
+++ tests/model_specs/session_tests.js
@@ -0,0 +1,51 @@
+'use strict';
+var Session = require('../../lib').Session;
+var expect = require('chai').expect;
+var dbUri = 'mongodb://localhost/hoist-model-test';
+var mongoose = require('mongoose');
+require('../bootstrap');
+/*jshint -W030 */
+describe('Application', function () {
+  before(function (done) {
+
+    if (mongoose.connection.db) {
+      return done();
+    }
+    mongoose.connect(dbUri, done);
+  });
+  after(function (done) {
+    Session.remove({}, function () {
+      mongoose.disconnect(function () {
+        delete mongoose.connection.db;
+        done();
+      });
+    });
+  });
+  describe('on save', function () {
+    var saved;
+    before(function () {
+      saved = new Session({
+        application: 'appid',
+        environment: 'live'
+      }).saveAsync();
+    });
+    it('should save ok', function () {
+      return expect(saved).to.be.fulfilled;
+    });
+    it('should not have modified date', function () {
+      return saved.then(function (session) {
+        expect(session.updatedAt).to.exist;
+      });
+    });
+    it('should not have created date', function () {
+      return saved.then(function (session) {
+        expect(session.createdAt).to.exist;
+      });
+    });
+    it('should have a short id', function () {
+      return saved.then(function (session) {
+        expect(session._id.length).to.eql(20);
+      });
+    });
+  });
+});
