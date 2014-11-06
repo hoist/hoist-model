b2421e95c907cfad87e45ba9ff101fd6297f3a3b
Owen Evans <owen@bgeek.net>
2014-11-07 12:07:47 +1300

    fix members->user references

--- tests/model_specs/app_user_tests.js
+++ tests/model_specs/app_user_tests.js
@@ -22,9 +22,9 @@ describe('AppUser', function () {
     });
   });
   describe('validation', function () {
-    var member;
+    var appUser;
     before(function () {
-      member = new AppUser({
+      appUser = new AppUser({
         application:'appid',
         environment:'live'
       });
@@ -32,81 +32,81 @@ describe('AppUser', function () {
     describe('save', function () {
       var saved;
       before(function () {
-        saved = member.saveAsync();
+        saved = appUser.saveAsync();
       });
       it('is saved', function () {
         return expect(saved).to.be.fulfilled;
       });
       it('has id',function(){
-        return expect(saved.then(function(member){
-          return member._id.length;
+        return expect(saved.then(function(appUser){
+          return appUser._id.length;
         })).to.become(20);
       });
     });
     describe('application',function(){
       afterEach(function(){
-        member.application = 'appid';
+        appUser.application = 'appid';
       });
       it('is required',function(){
-        member.application = null;
-        return member.validateAsync().then(function () {
+        appUser.application = null;
+        return appUser.validateAsync().then(function () {
           //fail here;
           expect(false).to.be.true();
         }).catch(function (err) {
           expect(err).to.be.instanceof(mongoose.Error.ValidationError);
           /* jshint -W030 */
           expect(err.errors.application).to.exist;
-          expect(err.errors.application.message).to.eql('members must belong to an application');
+          expect(err.errors.application.message).to.eql('users must belong to an application');
         });
       });
     });
      describe('environment',function(){
       afterEach(function(){
-        member.environment = 'live';
+        appUser.environment = 'live';
       });
       it('is required',function(){
-        member.environment = null;
-        return member.validateAsync().then(function () {
+        appUser.environment = null;
+        return appUser.validateAsync().then(function () {
           //fail here;
           expect(false).to.be.true();
         }).catch(function (err) {
           expect(err).to.be.instanceof(mongoose.Error.ValidationError);
           /* jshint -W030 */
           expect(err.errors.environment).to.exist;
-          expect(err.errors.environment.message).to.eql('members must belong to a valid environment');
+          expect(err.errors.environment.message).to.eql('users must belong to a valid environment');
         });
       });
       it('it must be a valid environment',function(){
-        member.environment = 'random';
-        return member.validateAsync().then(function () {
+        appUser.environment = 'random';
+        return appUser.validateAsync().then(function () {
           //fail here;
           expect(false).to.be.true();
         }).catch(function (err) {
           expect(err).to.be.instanceof(mongoose.Error.ValidationError);
           /* jshint -W030 */
           expect(err.errors.environment).to.exist;
-          expect(err.errors.environment.message).to.eql('members must belong to a valid environment');
+          expect(err.errors.environment.message).to.eql('users must belong to a valid environment');
         });
       });
     });
     describe('email', function () {
       beforeEach(function () {
-        member.emailAddresses = [];
+        appUser.emailAddresses = [];
       });
       afterEach(function(){
-        member.emailAddresses = [];
+        appUser.emailAddresses = [];
       });
       it('allows valid email', function () {
-        member.emailAddresses.push({
+        appUser.emailAddresses.push({
           address: 'test@hoi.io'
         });
-        return expect(member.validateAsync()).to.be.fulfilled;
+        return expect(appUser.validateAsync()).to.be.fulfilled;
       });
       it('rejects invalid email', function () {
-        member.emailAddresses.push({
+        appUser.emailAddresses.push({
           address: '3'
         });
-        return member.validateAsync().then(function () {
+        return appUser.validateAsync().then(function () {
           //fail here;
           expect(false).to.be.true();
         }).catch(function (err) {
@@ -118,9 +118,9 @@ describe('AppUser', function () {
 
       });
        it('rejects blank email', function () {
-        member.emailAddresses.push({
+        appUser.emailAddresses.push({
         });
-        return member.validateAsync().then(function () {
+        return appUser.validateAsync().then(function () {
           //fail here;
           expect(false).to.be.true();
         }).catch(function (err) {
