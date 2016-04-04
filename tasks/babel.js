39626312a413710adacaeef51fddaf28968d2bb3
Owen Evans <owen@bgeek.net>
2016-04-04 20:38:04 +1200

    fix babel task

--- tasks/babel.js
+++ tasks/babel.js
@@ -3,17 +3,9 @@ const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 const path = require('path');
 gulp.task('babel', () => {
-  return gulp
-    .src('src')
+  return gulp.src(process.cwd() + '/src/**/*.js')
     .pipe(sourcemaps.init())
-    .pipe(babel({
-      "presets": ["es2015"]
-    }))
-    .pipe(sourcemaps.write('maps', {
-      includeContent: false,
-      sourceRoot: function (file) {
-        return path.relative(path.resolve(process.cwd(), './lib/maps'), path.dirname(file.path));
-      }
-    }))
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('lib'));
 });
