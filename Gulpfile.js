b710169699f68084f638b87652184cf59870f9fc
Owen Evans <owen@bgeek.net>
2016-04-04 14:30:42 +1200

    fix build

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,4 +4,3 @@ var runSequence = require('run-sequence');
 require('require-dir')('./tasks');
 gulp.task('default', ['build', 'test']);
 gulp.task('build', ['babel']);
-gulp.task('clean', ['clean-dist', 'clean-docs']);
